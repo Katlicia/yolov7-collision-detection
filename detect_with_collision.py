fd642626aa39601cd319804aec7f22fd8c87ba4b
Yusuf Ustaoğlu <110745706+Katlicia@users.noreply.github.com>
2024-08-23 21:33:05 +0300

    Add files via upload

---
+++ detect_with_collision.py
@@ -0,0 +1,255 @@
+import argparse
+import time
+from pathlib import Path
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+
+def iou(box1, box2):
+    x1, y1, x2, y2 = box1
+    x1_, y1_, x2_, y2_ = box2
+    xi1 = max(x1, x1_)
+    yi1 = max(y1, y1_)
+    xi2 = min(x2, x2_)
+    yi2 = min(y2, y2_)
+    inter_area = max(0, xi2 - xi1) * max(0, yi2 - yi1)
+    box1_area = (x2 - x1) * (y2 - y1)
+    box2_area = (x2_ - x1_) * (y2_ - y1_)
+    union_area = box1_area + box2_area - inter_area
+    return inter_area / union_area
+
+def get_group_boxes(boxes):
+    groups = []
+    for box in boxes:
+        added = False
+        for group in groups:
+            if any(iou(box, b) > 0 for b in group):
+                group.append(box)
+                added = True
+                break
+        if not added:
+            groups.append([box])
+    
+    merged_groups = []
+    while groups:
+        current_group = groups.pop(0)
+        group_merged = False
+        for i in range(len(groups)):
+            if any(iou(box1, box2) > 0 for box1 in current_group for box2 in groups[i]):
+                groups[i].extend(current_group)
+                group_merged = True
+                break
+        if not group_merged:
+            merged_groups.append(current_group)
+    
+    return merged_groups
+
+def detect(save_img=False):
+    source, weights, view_img, save_txt, imgsz, trace = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size, not opt.no_trace
+    save_img = not opt.nosave and not source.endswith('.txt')
+    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
+        ('rtsp://', 'rtmp://', 'http://', 'https://'))
+
+    save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)
+
+    set_logging()
+    device = select_device(opt.device)
+    half = device.type != 'cpu'
+
+    model = attempt_load(weights, map_location=device)
+    stride = int(model.stride.max())
+    imgsz = check_img_size(imgsz, s=stride)
+
+    if trace:
+        model = TracedModel(model, device, opt.img_size)
+
+    if half:
+        model.half()
+
+    classify = False
+    if classify:
+        modelc = load_classifier(name='resnet101', n=2)
+        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+
+    vid_path, vid_writer = None, None
+    if webcam:
+        view_img = check_imshow()
+        cudnn.benchmark = True
+        dataset = LoadStreams(source, img_size=imgsz, stride=stride)
+    else:
+        dataset = LoadImages(source, img_size=imgsz, stride=stride)
+
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))
+
+    old_img_w = old_img_h = imgsz
+    old_img_b = 1
+
+    t0 = time.time()
+    for path, img, im0s, vid_cap in dataset:
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()
+        img /= 255.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        if device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+            old_img_b = img.shape[0]
+            old_img_h = img.shape[2]
+            old_img_w = img.shape[3]
+            for i in range(3):
+                model(img, augment=opt.augment)[0]
+
+        t1 = time_synchronized()
+        pred = model(img, augment=opt.augment)[0]
+        t2 = time_synchronized()
+
+        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        t3 = time_synchronized()
+
+        if classify:
+            pred = apply_classifier(pred, modelc, img, im0s)
+
+        for i, det in enumerate(pred):
+            if webcam:
+                p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
+            else:
+                p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+
+            p = Path(p)
+            save_path = str(save_dir / p.name)
+            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]
+
+            if len(det):
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+                founded_classes = {}
+                boxes = []
+
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()
+                    class_index = int(c)
+                    count_of_object = int(n)
+                    founded_classes[names[class_index]] = int(n)
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "
+
+                for *xyxy, conf, cls in reversed(det):
+                    if save_txt:
+                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()
+                        line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)
+                        with open(txt_path + '.txt', 'a') as f:
+                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                    boxes.append(xyxy)
+                    if save_img or view_img:
+                        label = f'{names[int(cls)]} {conf:.2f}'
+                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
+
+                grouped_boxes = get_group_boxes(boxes)
+                total_people = sum(len(group) for group in grouped_boxes)  # Toplam insan sayısını hesapla
+
+                red = 0, 0, 255
+                green = 0, 255, 0
+                orange = 0, 127, 255
+
+                # Toplam insan sayısını sağ üst köşeye ekle
+                if total_people >= 10:
+                    cv2.putText(im0, f'insan: {total_people}', (im0.shape[1] - 200, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, red, 2)
+                else:
+                    cv2.putText(im0, f'insan: {total_people}', (im0.shape[1] - 200, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, green, 2)
+
+                for group in grouped_boxes:
+                    box_count = len(group)
+
+                    x_min = min([box[0] for box in group])
+                    y_min = min([box[1] for box in group])
+                    x_max = max([box[2] for box in group])
+                    y_max = max([box[3] for box in group])
+
+
+                    label = f'{box_count} insan'
+                    if box_count >= 5:
+                        plot_one_box([x_min, y_min, x_max, y_max], im0, color=red, line_thickness=2)
+                        cv2.putText(im0, label, (int(x_min), int(y_min) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, red, 2)
+                    elif box_count == 4 or box_count == 3:
+                        plot_one_box([x_min, y_min, x_max, y_max], im0, color=orange, line_thickness=2)
+                        cv2.putText(im0, label, (int(x_min), int(y_min) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, orange, 2)
+                    elif box_count == 2:
+                        plot_one_box([x_min, y_min, x_max, y_max], im0, color=green, line_thickness=2)
+                        cv2.putText(im0, label, (int(x_min), int(y_min) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, green, 2)
+
+            print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
+
+            if view_img:
+                cv2.imshow(str(p), im0)
+                cv2.waitKey(1)
+
+            if save_img:
+                if dataset.mode == 'image':
+                    cv2.imwrite(save_path, im0)
+                else:
+                    if vid_path != save_path:
+                        vid_path = save_path
+                        if isinstance(vid_writer, cv2.VideoWriter):
+                            vid_writer.release()
+                        if vid_cap:
+                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                            save_path = str(Path(save_path).with_suffix('.mp4'))
+                            vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*opt.fourcc), fps, (w, h))
+                        else:
+                            vid_writer = None
+                    if vid_writer:
+                        vid_writer.write(im0)
+
+    if save_txt or save_img:
+        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
+        print(f"Results saved to {save_dir}{s}")
+
+    print(f'Done. ({time.time() - t0:.3f}s)')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model path(s)')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in txt labels')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
+    parser.add_argument('--fourcc', type=str, default='mp4v', help='fourcc codec')
+    opt = parser.parse_args()
+    print(opt)
+
+    check_requirements(exclude=('pycocotools', 'thop'))
+
+    with torch.no_grad():
+        if opt.update:
+            for opt.weights in ['yolov7.pt']:
+                detect()
+                strip_optimizer(opt.weights)
+        else:
+            detect()
